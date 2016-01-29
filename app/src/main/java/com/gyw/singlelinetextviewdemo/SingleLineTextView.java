118e1d77d17fcc4d58e1c1f8a36c919a6ac36c6e
gyw520gyw <gyw0829@foxmail.com>
2016-01-29 11:44:41 +0800

    update rect

--- app/src/main/java/com/gyw/singlelinetextviewdemo/SingleLineTextView.java
+++ app/src/main/java/com/gyw/singlelinetextviewdemo/SingleLineTextView.java
@@ -11,100 +11,99 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.widget.TextView;
 
+/**
+ * 单行的TextView
+ */
 public class SingleLineTextView extends TextView {
 
-	private String textContent;
-	private int textSize;
-	private Paint paint;
-	private Rect rect;
-	
+    private String textContent;
+    private int textSize;
+    private int textColor;
+    private Paint paint;
 
-	public SingleLineTextView(Context context) {
-		this(context, null);
-	}
+    private float textWidth;
+    private float textHeight;
 
-	public SingleLineTextView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
 
-	public SingleLineTextView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
+    public SingleLineTextView(Context context) {
+        this(context, null);
+    }
 
-		TypedArray ta = context.obtainStyledAttributes(attrs,
-				R.styleable.SingleLineTextView, defStyle, 0);
+    public SingleLineTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
-		final int N = ta.getIndexCount();
+    public SingleLineTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
 
-		for (int i = 0; i < N; i++) {
-			int attr = ta.getIndex(i);
+        TypedArray ta = context.obtainStyledAttributes(attrs,
+                R.styleable.SingleLineTextView, defStyle, 0);
 
-			switch (attr) {
-			
-			case R.styleable.SingleLineTextView_textContent:
-				textContent = ta.getString(attr);
-				break;
+        //获取文本
+        textContent = ta.getString(R.styleable.SingleLineTextView_textContent);
+        //字体大小
+        textSize = ta.getDimensionPixelSize(R.styleable.SingleLineTextView_textSize, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+        //字体颜色
+        textColor = ta.getColor(R.styleable.SingleLineTextView_textColor, Color.BLACK);
 
-			case R.styleable.SingleLineTextView_textSize:
-				textSize = ta.getDimensionPixelSize(attr, (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
-				break;
-			}
-		}
+        ta.recycle();
 
-		ta.recycle(); 
-		
-		paint = new Paint();
+        initPaint();
 
-		rect = new Rect();
-		paint.setTextSize(textSize);
-		paint.getTextBounds(textContent, 0, textContent.length(), rect);
 
-		Log.d("gyw ", "rect.width() :" + rect.width());
-	}
 
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    }
 
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+    //初始化画笔
+    private void initPaint() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        //获取文本的宽度
+        textWidth = paint.measureText(textContent);
+        //文本的高度
+        textHeight = paint.descent() - paint.ascent();
+    }
 
-		int widthsize = MeasureSpec.getSize(widthMeasureSpec);
-		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-		int width;
-		int height;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-		if (widthMode == MeasureSpec.EXACTLY) {
-			width = widthsize;
-		} else {
-			width = getPaddingLeft() + rect.width() + getPaddingRight();
-		}
+        int widthsize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-		if (heightMode == MeasureSpec.EXACTLY) {
-			height = heightSize;
-		} else {
-			height = getPaddingTop() + rect.height() + getPaddingBottom();
-		}
+        int width;
+        int height;
 
-		setMeasuredDimension(width, height);
-	}
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthsize;
+        } else {
+            width = getPaddingLeft() + (int)(textWidth+0.5) + getPaddingRight();
+        }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		Log.d("gyw ", "getWidth() :" + getWidth());
-		paint.setColor(Color.TRANSPARENT);
-		canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint);
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else {
+            height = getPaddingTop() + (int)(textHeight+0.5) + getPaddingBottom();
+        }
 
-		paint.setColor(Color.BLACK);
+        setMeasuredDimension(width, height);
+    }
 
-		Log.d("gyw ", "width "+paint.measureText(textContent) + " rect.width() : " + rect.width());
+    @Override
+    protected void onDraw(Canvas canvas) {
 
-		while(rect.width() > getWidth()) {
-			paint.setTextSize(textSize--);
-			paint.getTextBounds(textContent, 0, textContent.length(), rect);
-		}
+        //如果文本的宽度大于显示的宽度,将字体大小减小
+        while (textWidth > getWidth()) {
+            paint.setTextSize(textSize--);
+            textWidth = paint.measureText(textContent);
+        }
 
-		float textHeight = paint.descent() + paint.ascent();
-		float textBaseline = (getHeight() - textHeight) / 2.0f;
-		canvas.drawText(textContent, (getWidth()  - rect.width()) / 2.0f, textBaseline , paint);
-	}
+        //画出文本
+        float textBaseline = (getHeight() - (paint.descent() + paint.ascent())) / 2.0f;
+        canvas.drawText(textContent, (getWidth() -  textWidth) / 2.0f, textBaseline, paint);
+    }
 }
